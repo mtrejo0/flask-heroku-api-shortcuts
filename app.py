c7f3310f86672d178c6e310a444eada2f21ad94e
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2022-10-08 19:39:17 -0400

    new text

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
@@ -12,29 +12,12 @@ def test():
     }
     return jsonify(response)
     
-@app.route('/multiply_by_2/<number>')
-def multiply_by_2(number):
+@app.route('/google')
+def google():
     try:
-        number = int(number)
-        response = {
-            "message": "Here is the response!",
-            "res": number * 2 
-        }
+        response =  f'The google stock price is 123'
         return jsonify(response)
-    except Exception as e:
-        print(e)
-        return jsonify({"error": 'Failed'}), 400
-
-@app.route('/multiply_x_times_y', methods = ['POST'])
-def multiply_x_times_y():
-    res = request.get_json()
-
-    try:
-        response = {
-            "message": "Here is the response!",
-            "res": res["x"] * res["y"]
-        }
-        return jsonify(response)
+        
     except Exception as e:
         print(e)
         return jsonify({"error": 'Failed'}), 400
