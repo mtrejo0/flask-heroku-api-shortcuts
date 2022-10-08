7f0fce7f826fd8e8e9cf50ea93c22f1e6c9aaa99
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2022-10-08 13:41:04 -0400

    add to heroku

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+
+
 @app.route('/')
 def test():
         
@@ -9,10 +11,11 @@ def test():
         "message": "it works!"
     }
     return jsonify(response)
+    
 @app.route('/multiply_by_2/<number>')
 def multiply_by_2(number):
-    
     try:
+        number = int(number)
         response = {
             "message": "Here is the response!",
             "res": number * 2 
@@ -21,5 +24,20 @@ def multiply_by_2(number):
     except Exception as e:
         print(e)
         return jsonify({"error": 'Failed'}), 400
+
+@app.route('/multiply_x_times_y', methods = ['POST'])
+def multiply_x_times_y():
+    res = request.get_json()
+
+    try:
+        response = {
+            "message": "Here is the response!",
+            "res": res["x"] * res["y"]
+        }
+        return jsonify(response)
+    except Exception as e:
+        print(e)
+        return jsonify({"error": 'Failed'}), 400
+
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, port=5000)
+    app.run(debug=True, threaded=True, port=8080)
