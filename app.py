44a2d6df391431c53fc127aa340474f7cb42d7c5
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2022-10-08 19:39:56 -0400

    Revert "new text"
    This reverts commit c7f3310f86672d178c6e310a444eada2f21ad94e.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
@@ -12,12 +12,29 @@ def test():
     }
     return jsonify(response)
     
-@app.route('/google')
-def google():
+@app.route('/multiply_by_2/<number>')
+def multiply_by_2(number):
     try:
-        response =  f'The google stock price is 123'
+        number = int(number)
+        response = {
+            "message": "Here is the response!",
+            "res": number * 2 
+        }
         return jsonify(response)
-        
+    except Exception as e:
+        print(e)
+        return jsonify({"error": 'Failed'}), 400
+
+@app.route('/multiply_x_times_y', methods = ['POST'])
+def multiply_x_times_y():
+    res = request.get_json()
+
+    try:
+        response = {
+            "message": "Here is the response!",
+            "res": res["x"] * res["y"]
+        }
+        return jsonify(response)
     except Exception as e:
         print(e)
         return jsonify({"error": 'Failed'}), 400
